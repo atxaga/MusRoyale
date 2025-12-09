48aaa724efe2aa31978e208545ed8c291bf24ad4
atxaga <atxaga.iker65@gmail.com>
2025-12-09 11:01:40 +0100

    footer fragment

---
+++ app/src/main/java/com/example/musroyale/FooterFragment.kt
@@ -0,0 +1,27 @@
+package com.example.musroyale
+
+import android.app.Fragment
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.musroyale.databinding.FragmentFooterBinding
+import com.example.musroyale.databinding.FragmentHeaderBinding
+import com.example.musroyale.databinding.FragmentPartidaBinding
+import com.example.musroyale.databinding.FragmentSartuBinding
+
+
+class FooterFragment: Fragment() {
+    private lateinit var binding: FragmentFooterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFooterBinding.inflate(inflater, container, false)
+
+
+        return binding.root
+    }
+}
